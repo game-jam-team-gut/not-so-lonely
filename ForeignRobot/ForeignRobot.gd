8a83e67892ac600c780e79813d1cd95d90bb4041
Mateusz Grzonka <alpinus4@gmail.com>
2021-09-18 17:38:35 +0200

    More interesting foreign robots movement mechanics

--- ForeignRobot/ForeignRobot.gd
+++ ForeignRobot/ForeignRobot.gd
@@ -1,30 +1,49 @@
 extends RigidBody2D
 
-var direction_to_go = null
+
+
+var go = 0
+var position_to_go = null
+
 var rng = RandomNumberGenerator.new()
+var state_time #constant for the whole lifetime
+
 var camera #set by spawner 
+
 var movement_force_max = 12
 var movement_force_min = 5
 
 func _ready():
-	pass
+	rng.randomize()
+	state_time = rng.randi_range(500, 10000)
 
 func idle():
 	pass # do nothing XD
 
 func choose_direction():
-	var move_distance = camera.get_viewport().size.length()
 	rng.randomize()
-	var signs = [1, -1]
-	var rand_sign = Vector2(signs[rng.randi_range(0,1)], signs[rng.randi_range(0,1)])
-	rng.randomize()
-	var delta = Vector2(rng.randf_range(1,2), rng.randf_range(1,2))
-	direction_to_go = camera.get_camera_position() + move_distance * delta * rand_sign
+	match rng.randi_range(1,2):
+		1:
+			var move_distance = camera.get_viewport().size.length()
+			rng.randomize()
+			var signs = [1, -1]
+			var rand_sign = Vector2(signs[rng.randi_range(0,1)], signs[rng.randi_range(0,1)])
+			rng.randomize()
+			var delta = Vector2(rng.randf_range(1,2), rng.randf_range(1,2))
+			position_to_go = camera.global_position + move_distance * delta * rand_sign
+		2:
+			if camera.global_position.distance_to(position) < camera.get_viewport().size.length() / 3:
+				position_to_go = position + (position - camera.global_position)
+			else:
+				position_to_go = camera.global_position + Vector2(rng.randf_range(-100, 100), rng.randf_range(-100, 100))
 
 func go():
+	go += 1
+	if (go > 0):
+		print(name + " GO " + str(go))
 	rng.randomize()
-	var strength = (direction_to_go - position).normalized() * rng.randi_range(movement_force_min,movement_force_max)
-	add_force(Vector2.ZERO, strength * 2)
+	var strength = (position_to_go - position).normalized() * rng.randi_range(movement_force_min, movement_force_max)
+	add_force(Vector2.ZERO, strength)
 	add_central_force(strength)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
