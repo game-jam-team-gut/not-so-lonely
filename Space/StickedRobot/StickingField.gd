ff67854c34e8fb7f9e8cc57d9d29a2dc29e54f57
Mateusz Grzonka <alpinus4@gmail.com>
2021-09-19 16:21:50 +0200

    Add destroying sticking robots

---
+++ Space/StickedRobot/StickingField.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+#to remove later, debug
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if (event.is_pressed() and event.button_index == BUTTON_MIDDLE):
+			get_parent().destroy()
